1344de97141e2bcceaae10edad496c4137af6c3a
Sander de Vos <sander@orderchamp.com>
2021-01-14 10:09:13 +0100

    All optional address fields are now nullable

--- src/Data/RecipientData.php
+++ src/Data/RecipientData.php
@@ -5,17 +5,17 @@ namespace Soved\Laravel\Sendcloud\Data;
 class RecipientData extends Data
 {
     public string $name;
-    public string $company_name;
+    public ?string $company_name;
     public string $address;
-    public string $address_2;
+    public ?string $address_2;
     public ?string $house_number;
     public string $city;
     public string $postal_code;
-    public string $to_post_number;
+    public ?string $to_post_number;
     public string $country;
-    public string $country_state;
-    public string $telephone;
-    public string $email;
+    public ?string $country_state;
+    public ?string $telephone;
+    public ?string $email;
 
     public array $required = [
         'name',
--- src/Data/SenderData.php
+++ src/Data/SenderData.php
@@ -5,15 +5,15 @@ namespace Soved\Laravel\Sendcloud\Data;
 class SenderData extends Data
 {
     public string $from_name;
-    public string $from_company_name;
+    public ?string $from_company_name;
     public string $from_address_1;
-    public string $from_address_2;
+    public ?string $from_address_2;
     public ?string $from_house_number;
     public string $from_city;
     public string $from_postal_code;
     public string $from_country;
-    public string $from_telephone;
-    public string $from_email;
+    public ?string $from_telephone;
+    public ?string $from_email;
 
     public array $required = [
         'from_name',
